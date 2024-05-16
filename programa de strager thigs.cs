0e3484f9bef3d31a6960ddc071d3ffc120eb62a7
YOJAN <gyojanfelipe@gmail.com>
2024-05-16 15:13:43 -0500

    programa stranger thigs
    creación y solución problema de stranger thigs

---
+++ programa de strager thigs.cs
@@ -0,0 +1,17 @@
+/******************************************************************************
+autores: cesar esteban 
+        yojan garcia
+fecha:16-05-2024
+descripcion:los personajes  de stranger thigs necesitan un programa en c# para 
+registrar los encuentros con criaturas del mundo como el demogorgon .Completa el espacio
+en el blanoc para imprimir el nombre de la criatura en la consola
+
+*******************************************************************************/
+using System;
+class programa
+{
+  static void Main() {
+      string nombreCriatura = "demogorgon";
+    Console.WriteLine("el nombre de la criatura es: " + nombreCriatura);
+  }
+}
