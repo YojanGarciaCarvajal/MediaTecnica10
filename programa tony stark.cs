d0cc2bae7df0758af5e1d49491c509b7b993abec
YOJAN <gyojanfelipe@gmail.com>
2024-05-16 14:53:41 -0500

    Creación código Tony stark
    crear archivo programa y solución necesidad Tony stark

---
+++ programa tony stark.cs
@@ -0,0 +1,17 @@
+/******************************************************************************
+autor:yojan felipe garcia carvajal
+fecha: 16-05-2024
+descripcion: tony stark esta desarrollando un sistema de seguridad para su mansion 
+utilizando C#. completa el espacio en blanco para limpiar la pantalla antes de 
+mostrar el mensaje de "acceso denegado"
+*******************************************************************************/
+using System;
+class Program {
+    
+  static void Main()
+  {
+    Console.Clear();
+    Console.WriteLine("Acceso denegado");
+    
+  }
+}
