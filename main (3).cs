037c3e685df757b24c6e5819bf0b69166d1751dd
YOJAN <gyojanfelipe@gmail.com>
2024-05-22 16:52:18 -0500

    problema tony stark
    creacion y solucion del problema tony stark
    no quedo finalizado.

---
+++ main (3).cs
@@ -0,0 +1,25 @@
+/*********************************************************************
+AUTOR:yojan felipe garcia carvajal 
+FECHA:22-05-2024
+DESCRIPCION:  TONY STARK, IRON MAN, QUIERE SABER CUANTO 
+              DINERO DEBE TENER PARA COMPRAR X COMILLAS c/u A $300, PERO EN LA TIENDA
+              LE DAN DESCUENTOS SEGUN CANTIDAD SI COMPRA:
+              1 SIN DESCUENTOS
+              2 DESCUENTO DEL 30%
+              3 DESCUENTO DEL 50$
+**********************************************************************/
+using System;
+
+class Program{
+    static void Main(){
+      int sinDescuento;
+      int descuento;
+      int valorDelTornilloIndividual = 100
+      
+      Console.WriteLine("digitar el valor del tornillo individual " );
+      valorDelTornilloIndividual = Int32.Parse(Console.ReadLine());
+      
+      if 
+    
+    }
+}
